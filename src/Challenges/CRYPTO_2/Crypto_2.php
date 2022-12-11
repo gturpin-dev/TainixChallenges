4ca1972a6aaab2b1f59b14f939131a5a4c5fc65b
Guillaume <guillaume.turpin45@gmail.com>
2022-12-11 16:21:54 +0100

    :construction: WIP Crypto challenge

---
+++ src/Challenges/CRYPTO_2/Crypto_2.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Gturpin\TainixChallenges\Challenges\CRYPTO_2;
+
+use Gturpin\TainixChallenges\Challenge;
+
+/**
+ * @link https://tainix.fr/challenge/Braquage-du-coffre
+ */
+final class Crypto_2 extends Challenge {
+	
+	public function solve() : mixed {
+		
+	}
+}
