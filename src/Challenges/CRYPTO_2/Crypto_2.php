48312d9a61dc1ab3ffbd456cf124b054e18c41a8
Guillaume <guillaume.turpin45@gmail.com>
2022-12-11 18:13:45 +0100

    :sparkles: Make challenge CRYPTO_2

--- src/Challenges/CRYPTO_2/Crypto_2.php
+++ src/Challenges/CRYPTO_2/Crypto_2.php
@@ -10,6 +10,24 @@ use Gturpin\TainixChallenges\Challenge;
 final class Crypto_2 extends Challenge {
 	
 	public function solve() : mixed {
-		
+		$combinaison = $this->data['depart'] ?? 0;
+		$path        = $this->data['chemin'] ?? [];
+
+		foreach ( $path as $step ) {
+			$step_sign = substr( $step, 0, 1 );
+			$step_size = strlen( $step );
+			$operation = 10 ** ( $step_size - 1 );
+
+			switch ( $step_sign ) {
+				case '+':
+					$combinaison += $operation;
+					continue 2;
+				case '-':
+					$combinaison -= $operation;
+					continue 2;
+			}
+		}
+
+		return $combinaison;
 	}
 }
