849ef9972ae9b902d97e498b7afe013a5732822a
Guillaume <guillaume.turpin45@gmail.com>
2023-04-11 14:27:35 +0200

    :recycle: Refacto DIGITALART_1

--- src/Challenges/DIGITALART_1/Digitalart_1.php
+++ src/Challenges/DIGITALART_1/Digitalart_1.php
@@ -3,6 +3,7 @@
 namespace Gturpin\TainixChallenges\Challenges\DIGITALART_1;
 
 use Gturpin\TainixChallenges\Challenge;
+use Illuminate\Support\Collection;
 
 /**
  * @link TODO : add link to challenge here (https://tainix.fr/challenges/)
@@ -13,9 +14,9 @@ final class Digitalart_1 extends Challenge {
 	protected const ENABLE_LOG    = true;
 	
 	public function solve() : mixed {
-		$accounts = $this->data['accounts'] ?? [];
+		$accounts = new Collection( $this->data['accounts'] ?? [] );
 
-		$total_followers = array_reduce( $accounts, fn( $total, $account ) => $total + $account, 0 );
+		$total_followers = $accounts->sum();
 
 		return $total_followers;
 	}
