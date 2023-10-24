1c47439a5294b64f8ae24b70cfc7c63a05ea029b
Guillaume <guillaume.turpin45@gmail.com>
2023-10-24 08:58:43 +0200

    ✅ Add tests for RUBGY_2 solve

---
+++ tests/challenges/RUGBY_2/Rugby_2Test.php
@@ -0,0 +1,38 @@
+<?php
+
+use Gturpin\TainixChallenges\Challenges\RUGBY_2\Rugby_2;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group RUGBY_2
+ */
+class Rugby_2Test extends TestCase {
+	/**
+	 * Setup the test environment
+	 *
+	 * @return void
+	 */
+	public function setUp() : void {
+		parent::setUp();
+
+		$this->challenge = new Rugby_2( 'RUGBY_2' );
+	}
+
+	public function test_basic_solve() {
+		$this->challenge->set_data( [
+			'actions' => 'PEPPD'
+		] );
+		$expected = 17;
+
+		$this->assertSame( $expected, $this->challenge->solve() );
+	}
+
+	public function test_wrong_solve() {
+		$this->challenge->set_data( [
+			'actions' => 'EETDE'
+		] );
+		$expected = 23;
+
+		$this->assertNotSame( $expected, $this->challenge->solve() );
+	}
+}
