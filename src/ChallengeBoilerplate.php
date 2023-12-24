08de9fa8c98648a4d3a23eaed50672e58cb1fa96
Guillaume <guillaume.turpin45@gmail.com>
2023-12-24 11:08:38 +0100

    :recycle: Update Challenge boilerplate

--- src/ChallengeBoilerplate.php
+++ src/ChallengeBoilerplate.php
@@ -8,12 +8,11 @@ use Gturpin\TainixChallenges\Challenge;
  * @link TODO : add link to challenge here (https://tainix.fr/challenges/)
  */
 final class Challenge_Boilerplate extends Challenge {
-	
 	protected const USE_DATA_TEST = true;
 	protected const ENABLE_LOG    = true;
 	
 	public function solve() : mixed {
-		echo '<pre>' . print_r( $this->data, true ) . '</pre>';
+		dump( $this->data );
 		
 		die;
 	}
